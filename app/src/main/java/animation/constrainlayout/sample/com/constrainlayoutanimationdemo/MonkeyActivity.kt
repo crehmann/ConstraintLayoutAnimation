790ca5b0aecd9f25c553bfd36f6521f7669a28d7
crehmann <crehmann@users.noreply.github.com>
2018-05-21 14:04:41 +0200

    refactoring

--- app/src/main/java/animation/constrainlayout/sample/com/constrainlayoutanimationdemo/MonkeyActivity.kt
+++ app/src/main/java/animation/constrainlayout/sample/com/constrainlayoutanimationdemo/MonkeyActivity.kt
@@ -1,40 +1,38 @@
 package animation.constrainlayout.sample.com.constrainlayoutanimationdemo
 
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
 import android.support.v4.view.animation.FastOutSlowInInterpolator
 import android.support.v7.app.AppCompatActivity
 import android.transition.ChangeBounds
 import android.transition.TransitionManager
-import android.view.View
+import kotlinx.android.synthetic.main.activity_monkey_detail.*
 
 class MonkeyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_monkey)
+        setContentView(R.layout.activity_monkey_detail)
         configureAnimation()
     }
 
     private fun configureAnimation() {
-        var targetConstraintsApplied = false
-        val layout = findViewById<ConstraintLayout>(R.id.layout)
+        var endConstraintSetApplied = false
 
         val transition = ChangeBounds()
         transition.interpolator = FastOutSlowInInterpolator()
 
-        val initialConstraints = ConstraintSet()
-        initialConstraints.clone(layout)
+        val startConstraintSet = ConstraintSet()
+        startConstraintSet.clone(constraintLayout)
 
-        val targetConstrains = ConstraintSet()
-        targetConstrains.clone(this, R.layout.activity_monkey_detail)
+        val endConstraintSet = ConstraintSet()
+        endConstraintSet.clone(this, R.layout.activity_monkey)
 
-        findViewById<View>(R.id.layout).setOnClickListener {
-            TransitionManager.beginDelayedTransition(layout, transition)
-            val constraint = if (targetConstraintsApplied) initialConstraints else targetConstrains
-            constraint.applyTo(layout)
-            targetConstraintsApplied = !targetConstraintsApplied
+        constraintLayout.setOnClickListener {
+            TransitionManager.beginDelayedTransition(constraintLayout, transition)
+            val constraint = if (endConstraintSetApplied) startConstraintSet else endConstraintSet
+            constraint.applyTo(constraintLayout)
+            endConstraintSetApplied = !endConstraintSetApplied
         }
     }
 }
